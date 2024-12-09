aad31311f2bba47af256a3e7ef912d4e9ecf74d9
Yingshuo Ma <pandaawake@163.com>
2024-12-09 15:12:33 +0800

    testcases

---
+++ testcase/FilenameGuardFileterNew.java
@@ -0,0 +1,10 @@
+class FilenameGuardFileterNew {
+    private String guard(String filename) {
+        String guarded = filename.replace(":", "_");
+        guarded = FileSystems.getDefault().getPath(guarded).normalize().toString();
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("guarded " + filename + " to " + guarded);
+        }
+        return guarded;
+    }
+}
---
+++ testcase/FilenameGuardFileterOld.java
@@ -0,0 +1,9 @@
+class FilenameGuardFileterOld {
+    private String guard(String filename) {
+        String guarded = filename.replace(":", "_").replace("\\", "").replace("/", "");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("guarded " + filename + " to " + guarded);
+        }
+        return guarded;
+    }
+}
